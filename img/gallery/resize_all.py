b45a48ae5da894929fc83248563ca1d596288fca
Jimut Bahan Pal <jimutbahanpal@yahoo.com>
2023-12-28 06:00:46 +0530

    gallery modified

--- img/gallery/resize_all.py
+++ img/gallery/resize_all.py
@@ -11,6 +11,7 @@ from tqdm import tqdm
 from os import listdir
 from os.path import isfile, join
 from os import walk
+import matplotlib.pyplot as plt
 
 
 print(" Clearing all the *_thumb* files")
@@ -29,16 +30,41 @@ def reshape_image(img_name):
      
     print('Original Dimensions : ',img.shape)
      
-    scale_percent = 40 # percent of original size
-    print("RESCALING TO :=> ", scale_percent)
-    width = int(img.shape[1] * scale_percent / 100)
-    height = int(img.shape[0] * scale_percent / 100)
-    dim = (width, height)
+    # scale_percent = 40 # percent of original size
+    # print("RESCALING TO :=> ", scale_percent)
+    # width = int(img.shape[1] * scale_percent / 100)
+    # height = int(img.shape[0] * scale_percent / 100)
+    # dim = (width, height)
     # resize image
-    resized = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
+    # resized = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
+    SQUARE_ONE_SIDE = 1000
+    h,w,_ = img.shape
+    if h>SQUARE_ONE_SIDE and w>SQUARE_ONE_SIDE:
+        # do the stuff here
+        mid_h = int(h/2)
+        mid_w = int(w/2)
+        cropped_img = img[mid_h-int(SQUARE_ONE_SIDE/2):mid_h+int(SQUARE_ONE_SIDE/2), mid_w-int(SQUARE_ONE_SIDE/2):mid_w+int(SQUARE_ONE_SIDE/2)]
+        # plt.imshow(cropped_img)
+        # plt.show()
+    elif h>SQUARE_ONE_SIDE and w<SQUARE_ONE_SIDE:
+        # do the stuff here
+        mid_h = int(h/2)
+        cropped_img = img[mid_h-int(SQUARE_ONE_SIDE/2):mid_h+int(SQUARE_ONE_SIDE/2), 0:w]
+        # plt.imshow(cropped_img)
+        # plt.show()
+    elif h<SQUARE_ONE_SIDE and w>SQUARE_ONE_SIDE:
+        # do the stuff here
+        mid_w = int(w/2)
+        cropped_img = img[0:h, mid_w-int(SQUARE_ONE_SIDE/2):mid_w+int(SQUARE_ONE_SIDE/2)]
+        # plt.imshow(cropped_img)
+        # plt.show()
+    else:
+        cropped_img = img #cv2.resize(img, (1000, 1000), interpolation = cv2.INTER_AREA)
+        
      
-    print('Resized Dimensions : ',resized.shape)
-    cv2.imwrite(str(thumb_name+"_thumb."+exten),resized)
+    # print('Resized Dimensions : ',resized.shape)
+    # cv2.imwrite(str(thumb_name+"_thumb."+exten),resized)
+    cv2.imwrite(str(thumb_name+"_thumb."+exten),cropped_img)
 
 #print(f)
 for item in tqdm(f):
