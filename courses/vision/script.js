ff11677cdc738531651def597f70f58dd4ddad2f
Jimut123 <jimutbahanpal@yahoo.com>
2024-03-07 01:07:25 +0530

    offering updated

---
+++ courses/vision/script.js
@@ -0,0 +1,23 @@
+// Function to open a specific tab
+function openTab(tabName) {
+    // Hide all tab content
+    var tabcontent = document.getElementsByClassName("tabcontent");
+    for (var i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Deactivate all tab links
+    var tablinks = document.getElementsByClassName("tablink");
+    for (var i = 0; i < tablinks.length; i++) {
+        tablinks[i].classList.remove("active");
+    }
+
+    // Show the selected tab content
+    document.getElementById(tabName).style.display = "block";
+
+    // Activate the clicked tab link
+    event.currentTarget.classList.add("active");
+}
+
+// By default, open the first tab
+openTab('tab1');
